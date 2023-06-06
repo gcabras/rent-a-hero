cab144a1405122931a9dfd1a7add675f31d92ab6
Grazia Cabras <graziacabras.gcabras@gmail.com>
2023-06-06 14:03:16 +0200

    Add user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name username])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name username])
+  end
 end
